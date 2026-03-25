8d06918ab7982f1d4c0f67b2f6acaccad41f43ab
Sakhura <sabina.romero@gmail.com>
2026-03-24 22:34:09 -0300

    index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// Rutas
+const usuariosRoutes = require('./routes/usuarios');
+app.use('/api', usuariosRoutes);
+
+app.get('/', (req, res) => {
+    res.json({ message: 'API activa' });
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
