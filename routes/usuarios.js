920e87f5c37e06b04e78901a241ea005764b0327
Sakhura <sabina.romero@gmail.com>
2026-03-24 22:30:07 -0300

    usuario.js

---
+++ routes/usuarios.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../config/db');  
+
+//POST - Crear un nuevo usuario
+router.post('/usuarios', async (req, res) => {
+    const { nombre, correo } = req.body;
+
+    if (!nombre || !correo) {
+        return res.status(400).json({ error: 'Nombre y correo son requeridos' });
+    }
+
+    try{
+        const result = await pool.query(
+        'INSERT INTO usuarios (nombre, correo) VALUES ($1, $2) RETURNING *',
+        [nombre, correo]
+        );
+        res.status(201).json({ message: 'Usuario creado exitosamente', id: result.rows[0].id });
+    } catch (error) {
+
+        if(error.code === '23505') { // Código de error para violación de clave única
+            res.status(409).json({ error: 'El correo ya está registrado' });
+        }
+        res.status(500).json({ error: 'Error interno del servidor' });
+    }
+       
+    });
+
+    //PUT actualizar correo de un usuario
+router.put('/usuarios/:id', async (req, res) => {
+    const { id } = req.params;
+    const { correo } = req.body;
+
+    if (!correo) {
+        return res.status(400).json({ error: 'Correo es requerido' });
+    }
+    try{
+        const result = await pool.query(
+        'UPDATE usuarios SET correo = $1 WHERE id = $2 RETURNING id, nombre, correo',
+        [correo, id]
+        );
+        if (result.rowCount === 0) {
+            return res.status(404).json({ error: 'Usuario no encontrado id ' + id });
+        }
+        res.json({ message: 'Correo actualizado exitosamente', usuario: result.rows[0] });
+    }catch (error) {
+        if (error.code === '23505') { // Código de error para violación de clave única
+            res.status(409).json({ error: 'El correo ya está registrado' });
+        }
+        res.status(500).json({ error: 'Error interno del servidor' });
+    }
+});
+
+module.exports = router;
